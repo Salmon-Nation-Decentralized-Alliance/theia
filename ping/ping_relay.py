2d1090e17d2d124edbc8684f1448e5b03ca60e20
Theia <theia@eth7.net>
2022-01-03 21:09:08 +0000

    Add options to control how ping treats unique relay addresses

--- ping/ping_relay.py
+++ ping/ping_relay.py
@@ -36,7 +36,10 @@ def ping_relay(relay, timeout):
     address = relay["address"]
     port = relay["port"]
     magic = relay["network_magic"]
-    return cncli_ping(address, port, magic, timeout)
+    ping_result = cncli_ping(address, port, magic, timeout)
+    enriched = copy.deepcopy(relay)
+    enriched["ping_result"] = ping_result 
+    return enriched
 
 def relay_sort_key(relay):
     addr = relay["address"]
@@ -51,7 +54,7 @@ def relay_sort_key(relay):
         normalized_key = f"{p0:03d}{p1:03d}{p2:03d}{p3:03d}{port:05d}"
     elif ':' in addr: # ipv6
         # avoid mixing ipv4 and ipv6 addresses
-        normalized_key = f"999999999999:{addr}:::{port}"
+        normalized_key = f"z{addr}z{port}"
     return normalized_key
 
 def sort_relays(unsorted_relays):
@@ -59,6 +62,9 @@ def sort_relays(unsorted_relays):
 
 def discard_all_but_one_relays_which_share_address(relays_src):
     relays_sorted = sort_relays(relays_src)
+    #for relay in relays_sorted: 
+    #    print(relay["address"] + " " + str(relay["port"]))
+
     relays_dst = []
     addr_hash = {}
     for relay in relays_sorted:
@@ -71,6 +77,10 @@ def discard_all_but_one_relays_which_share_address(relays_src):
     return relays_dst
 
 if __name__ == "__main__":
+    VERBOSE_ERROR=0
+    VERBOSE_INFO=1
+    VERBOSE_DEBUG=2
+    default_verbose = VERBOSE_ERROR
     parser = argparse.ArgumentParser(description="Ping Pool - part of the Cardano Pool Reachability Dashboard (CardBoard) project")
     parser.add_argument("--version", action="version", version="1.0")
     parser.add_argument("--threads", type=int, default=100, help="max number of concurrent ping processes")
@@ -79,25 +89,24 @@ if __name__ == "__main__":
     # TODO: support writing to S3
     parser.add_argument("--dst-file", default="ping_results.json", help="output file: results of cncli ping")
     parser.add_argument("--dry-run", action="store_true", default=False, help="run, but don't ping anything. Output file will be empty.")
-
+    parser.add_argument("--unique-ip", action="store_true", default=False, help="ping each unique address only once, even if multiple relays share the address")
+    parser.add_argument("--verbose", action="count", default=default_verbose, help="increase verbosity")
     param = parser.parse_args()
     results = []
     time_start = time.time()
-
     with urlopen(param.src_url) as src_json:
         relays = json.load(src_json)
-
-        # At the time of this writing, 30% of relays share an IP address.
+        # At the time of this writing, 30% of relays share an IP address with another relay.
         # For the purpose of this project, we're mainly interested if the 
         # IP address is reachable, less interested in pinging multiple relays
         # behind a single IP address. 
         # Therefore, the following function ensures each IP address appears 
-        # only once in the list by discarding all-but-one relays which share an IP address
-        relays = discard_all_but_one_relays_which_share_address(relays)
-
-        for relay in relays:
-            print(relay["address"])
-
+        # only once in the list by discarding all-but-one relays which share an IP address.
+        if param.unique_ip:
+            relays = discard_all_but_one_relays_which_share_address(relays)
+        if param.verbose == VERBOSE_DEBUG:
+            for relay in relays: 
+                print(relay["address"] + " " + str(relay["port"]))
         futures = []
         with ProcessPoolExecutor(param.threads) as executor:
             if not param.dry_run:
@@ -105,12 +114,15 @@ if __name__ == "__main__":
                     futures.append(executor.submit(ping_relay, relay, param.timeout))
             for future in futures:
                 result = future.result()
-                print(result)
+                if param.verbose == VERBOSE_DEBUG:
+                    print(result)
                 results.append(result)
-
+        results = sort_relays(results)
         with open(param.dst_file, "w") as outfile:
             json.dump(results, outfile)
-            print(f"{len(results)} ping results written to '{param.dst_file}'")
+            if param.verbose >= VERBOSE_INFO:
+                print(f"{len(results)} ping results written to '{param.dst_file}'")
     time_end = time.time()
-    print(f"{(time_end-time_start):.2f}s: total time")
+    if param.verbose >= VERBOSE_INFO:
+        print(f"{(time_end-time_start):.2f}s: total time")
  
